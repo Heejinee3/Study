ed4c7b24e37ab96b37e0c8f3dcd932ab3fd8be92
Heejinee3 <102013918+Heejinee3@users.noreply.github.com>
2023-01-03 18:22:37 +0900

    Add files via upload

---
+++ frontend-shop/js/curtainMenu.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("overlay").style.width = "70%";
+  document.getElementById("overlayBg").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("overlay").style.width = "0%";
+  document.getElementById("overlayBg").style.width = "0%";
+}
